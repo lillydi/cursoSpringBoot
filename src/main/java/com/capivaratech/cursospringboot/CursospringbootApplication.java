e5e6c2b60ae1976f3c8102256636cc222d8d9c6a
Lillydi Cacere <lillydicacere@gmail.com>
2021-06-24 14:54:52 -0400

    Creating instancing operation

--- src/main/java/com/capivaratech/cursospringboot/CursospringbootApplication.java
+++ src/main/java/com/capivaratech/cursospringboot/CursospringbootApplication.java
@@ -1,13 +1,33 @@
 package com.capivaratech.cursospringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringbootApplication {
+import com.capivaratech.cursospringboot.domain.Categoria;
+import com.capivaratech.cursospringboot.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringbootApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
--- src/main/java/com/capivaratech/cursospringboot/resources/CategoriaResource.java
+++ src/main/java/com/capivaratech/cursospringboot/resources/CategoriaResource.java
@@ -1,9 +1,5 @@
 package com.capivaratech.cursospringboot.resources;
 
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
